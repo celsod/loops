26ac398712ba9637678400df6163c103b5cded0d
celsod <cdesouzajr@gmail.com>
2024-05-07 19:38:50 -0500

    Finishing off the chapter 3 tutorial for Rust.

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+fn main() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+    while index < 5 {
+        println!("The value is: {}; at index location: {}", a[index], index);
+        index += 1;
+    }
+
+    for element in a {
+        println!("The element value is: {}", element);
+    }
+
+    for number1 in (1..4).rev(){
+        println!("{}", number1);
+    }
+    println!("LIFTOFF!");
+}
